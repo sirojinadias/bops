d89b66881be9e77824f3c4c5ba271660d6c5c389
Joris <joris@saidani.org>
2020-06-25 16:31:42 +0200

    upgrade fidbak player

--- src/components/player.js
+++ src/components/player.js
@@ -8,7 +8,7 @@ const Player = ({ siteTitle }) => (
   <Section className="hero center">
     <div className="hero-body">
       <div className="container">
-        <iframe title="listentobops" width="100%" height="340" scrolling="no" frameborder="no" src="https://fidbak.audio/bops/embed/9bb57d3c8ab7bc72faa6b545"></iframe>
+        <iframe title="listentobops" width="100%" height="340" scrolling="no" frameborder="no" src="https://fidbak.audio/bops/embed/9bb57d3c8ab7bc72faa6b545/02fb4f"></iframe>
       </div>
     </div>
   </Section>
